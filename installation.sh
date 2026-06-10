7974aad7e2922c569dd58c9000b39f8c83ffbc8c
Adre ‎ <adre@Adres-MacBook-Air.local>
2026-06-10 18:53:51 +0800

    Updated installation script to fix awscli install

--- installation.sh
+++ installation.sh
@@ -3,7 +3,9 @@
 apt update
 apt install python3 python3-pip python3-venv -y
 apt install ansible -y
-apt install awscli -y
 pip install boto3 botocore --break-system-packages
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
 ansible-galaxy collection install amazon.aws
 ansible-galaxy collection install community.aws
